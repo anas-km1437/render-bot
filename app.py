882119e9430b089b95652036e4c5b0428c9f89ad
anas-km1437 <anas2001gamer@gmail.com>
2026-05-14 19:09:33 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,38 +6,66 @@ app = Flask(__name__)
 
 DATA_FILE = "data.json"
 
+# إنشاء الملف إذا غير موجود
 if not os.path.exists(DATA_FILE):
-    with open(DATA_FILE, "w") as f:
+
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
         json.dump([], f)
 
 
+# تحميل البيانات
 def load_data():
-    with open(DATA_FILE, "r") as f:
+
+    with open(DATA_FILE, "r", encoding="utf-8") as f:
         return json.load(f)
 
 
+# حفظ البيانات
 def save_data(data):
-    with open(DATA_FILE, "w") as f:
-        json.dump(data, f, indent=2)
+
+    with open(DATA_FILE, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=2
+        )
 
 
+# الصفحة الرئيسية
 @app.route("/")
 def home():
+
     return "Pinterest Collector API is running"
 
 
-# استقبال الرسائل من الإضافة
+# استقبال البيانات
 @app.route("/add", methods=["POST"])
 def add():
 
-    new_data = request.json
+    try:
 
-    data = load_data()
-    data.append(new_data)
+        new_data = request.json
 
-    save_data(data)
+        data = load_data()
 
-    return jsonify({"status": "saved"})
+        data.append(new_data)
+
+        save_data(data)
+
+        print("NEW DATA:", new_data)
+
+        return jsonify({
+            "status": "saved"
+        })
+
+    except Exception as e:
+
+        print("ERROR:", str(e))
+
+        return jsonify({
+            "error": str(e)
+        })
 
 
 # عرض البيانات
@@ -50,4 +78,8 @@ def logs():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+
+    app.run(
+        host="0.0.0.0",
+        port=10000
+    )
